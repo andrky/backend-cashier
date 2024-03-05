53a0ff0a8c934d4e91252b84802e6445247598b7
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-03-05 11:02:04 +0700

    connect to mongo db

--- app.js
+++ app.js
@@ -6,20 +6,32 @@
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
+// Import package
+// dotenv untuk koneksi dengan semua yang berhubungan dengan .env
+// mongoose untuk koneksi dengan database mongodb
 import express from 'express';
-import logger from 'morgan';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
 import indexRouter from './routes/index.js';
 
 const env = dotenv.config().parsed;
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
+
+// Koneksi ke database mongodb
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}/`, {
+	dbName: env.MONGODB_DB_NAME,
+});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+	console.log('Connected to MongoDB');
+});
 
 app.listen(env.APP_PORT, () => {
 	console.log(`Server running on port ${env.APP_PORT}`);
