711bf761500233aafe52209da32c41514ec15f21
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-03-06 11:47:03 +0700

    routes products

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import categories from './categories.js';
+import products from './categories.js';
 
 var router = express.Router();
 
 router.use('/categories', categories);
+
+router.use('/products', products);
 
 export default router;
---
+++ routes/products.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { index, store } from '../controllers/ProductController.js';
+
+var router = express.Router();
+
+// Get data categories from db
+router.get('/', index);
+
+// Post data category to db
+router.post('/', store);
+
+export default router;
