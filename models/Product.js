104c430a9aadc1b1e69c39e08e33f5d3954e4fed
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-03-06 11:44:41 +0700

    product model

---
+++ models/Product.js
@@ -0,0 +1,43 @@
+// Import library mongoose
+import mongoose from 'mongoose';
+
+// Buat model schema untuk field category
+const Schema = mongoose.Schema(
+	{
+		title: {
+			type: String,
+		},
+		thumbnail: {
+			type: String,
+		},
+		price: {
+			type: Number,
+		},
+		categoryId: {
+			type: mongoose.Schema.Types.ObjectId,
+		},
+		status: {
+			type: String,
+			enum: ['active', 'inactive'],
+			default: 'active',
+		},
+		createdAt: {
+			type: Number,
+		},
+		updatedAt: {
+			type: Number,
+		},
+	},
+	{
+		timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+	}
+);
+
+// Join dengan model Category
+Schema.virtual('categories', {
+	ref: 'category',
+	localField: 'categoryId',
+	foreignId: '_id',
+});
+
+export default mongoose.model('Product', Schema);
