95f109d9fe2a66e9bf83181430259afa79b4e583
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-03-09 10:33:09 +0700

    install cors

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import indexRouter from './routes/index.js';
+import cors from 'cors';
 
 const env = dotenv.config().parsed;
 var app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
